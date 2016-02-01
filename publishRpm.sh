d8bf3b561db17d685a9f235c3815de60f0556cb0
Brian Flannery <bflannery@caci.com>
2016-02-01 13:09:41 -0700

    Add publishRpm.sh

---
+++ publishRpm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo Sending RPM to nexus.
+
+vnext="1.0-2";
+nexusUrl="http://nexus.devops.geointservices.io" ;
+repoUrl="content/repositories/FADE-Releases";
+file="../artifacts/Mage-Server-1.0-1.x86_64.rpm";
+
+mvn deploy:deploy-file -Dfile=$file -DrepositoryId=nexus -Durl="$nexusUrl/$repoUrl" -DgroupId=mage -DartifactId=mage-server -Dversion=$vnext -DgeneratePom=true -Dpackaging=rpm -e
+

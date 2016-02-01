f5ea544f6d2639d83f854bbf2db38b5a58112f33
Brian Flannery <bflannery@caci.com>
2016-02-01 13:20:59 -0700

    Quiet

--- publishRpm.sh
+++ publishRpm.sh
@@ -2,7 +2,7 @@
 
 echo Sending RPM to nexus.
 
-vnext="1.0-39";
+vnext="1.0-40";
 nexusUrl="http://nexus.devops.geointservices.io" ;
 repoUrl="content/repositories/FADE-Releases";
 file="../artifacts/Mage-Server-1.0-1.x86_64.rpm";
